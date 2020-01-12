5cf998fa35f9d938696cec6960114345653cacc6
tarunshrm864 <tarunshrm864@gmail.com>
2020-01-12 21:54:16 +0530

    React Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
+import Sidebar from '../src/components/sidebar';
+import Homepage from './components/homepage';
+import Locations from './components/locations';
+import Resident from './components/resident';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/>
+    <React.Fragment>
+    <Sidebar/>
+      <div className="content">
+        <BrowserRouter>
+        
+        <Switch>
+          <Route
+            exact path="/"
+            component={Homepage}
+          />
+        <Route
+          path="/locations"
+          component={Locations}
+          />
+        <Route
+          path="/resident.js"
+          component={Resident} 
+        />
+        </Switch>
+        </BrowserRouter>
+            </div> 
+    </React.Fragment>
     </div>
   );
 }
 
 export default App;
+                    
